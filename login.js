85d62486cf857ea329e91f64922271a6ebe7e3d6
chris69914010 <165472442+chris69914010@users.noreply.github.com>
2024-03-30 16:52:33 +0630

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+
+    // Here, you would typically perform validation and authentication.
+    // For demonstration purposes, let's assume the login is successful.
+
+    // Redirect to another HTML page after successful login
+    window.location.href = "link.html"; // Change "welcome.html" to the desired URL
+}
---
+++ newtab.js
@@ -0,0 +1,56 @@
+document.getElementById('btn btn1').addEventListener('click', this.EvaElfie);
+document.getElementById('btn btn2').addEventListener('click', this.BellaDelphine);
+document.getElementById('btn btn3').addEventListener('click', this.MelodyMark);
+document.getElementById('btn btn4').addEventListener('click', this.MsPuiyi);
+document.getElementById('btn btn5').addEventListener('click', this.Naughtyblog);
+document.getElementById('btn btn6').addEventListener('click', this.NoodleMagazine);
+document.getElementById('btn btn7').addEventListener('click', this.Pornhits);
+document.getElementById('btn btn8').addEventListener('click', this.Porn00);
+document.getElementById('btn btn9').addEventListener('click', this.Xmovix);
+
+
+
+function EvaElfie(ev) {
+    console.log('EvaElfie');
+    window.open('https://mega.nz/folder/Ne03RKaa#00AWO1dXkuVhb80Dkpe2cA	');
+}
+
+function BellaDelphine(ev) {
+    console.log('BellaDelphine');
+    window.open('https://mega.nz/folder/ZrMSSbJJ#Vag5PVVuSpe6hLkRYWCiaA');
+}
+
+function MelodyMark(ev) {
+    console.log('MelodyMark');
+    window.open('https://mega.nz/folder/kZkHkTjA#PSC6Bg2J-eP6_ylGsHa3mA');
+}
+
+function MsPuiyi(ev) {
+    console.log('MsPuiyi');
+    window.open('https://mega.nz/folder/4W01BSpa#-xmtQ_lKJ2wL3Ex5lvDrNg');
+}
+
+function Naughtyblog(ev) {
+    console.log('Naughtyblog');
+    window.open('https://www.naughtyblog.org');
+}
+
+function NoodleMagazine(ev) {
+    console.log('NoodleMagazine');
+    window.open('https://noodlemagazine.com');
+}
+
+function Pornhits(ev) {
+    console.log('Pornhits');
+    window.open('https://www.pornhits.com');
+}
+
+function Porn00(ev) {
+    console.log('Porn00');
+    window.open('https://www.porn00.org');
+}
+
+function Xmovix(ev) {
+    console.log('Xmovix');
+    window.open('https://www.xmovix.net');
+}
